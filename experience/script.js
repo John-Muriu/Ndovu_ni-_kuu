65f66e22c10a8203066376bb967c9d2cdfd49877
john-muriu <johnmuriw@gmail.com>
2024-01-04 01:40:57 +0300

    commit

--- experience/script.js
+++ experience/script.js
@@ -35,7 +35,7 @@ var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
 (function(){
 var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
 s1.async=true;
-s1.src='https://embed.tawk.to/60df10bf7f4b000ac03ab6a8/1f9jlirg6';
+s1.src='https://embed.tawk.to/60df10bf7f4b00';
 s1.charset='UTF-8';
 s1.setAttribute('crossorigin','*');
 s0.parentNode.insertBefore(s1,s0);
@@ -65,7 +65,7 @@ document.onkeydown = function(e) {
 document.addEventListener('visibilitychange',
 function(){
     if(document.visibilityState === "visible"){
-        document.title = "Experience | Portfolio Jigar Sable";
+        document.title = "Experience |  john Muriu";
         $("#favicon").attr("href","/assets/images/favicon.png");
     }
     else {
